174de68878c8e9e98a20660ddafcfc75dc89f689
Giulliana Beatriz <giullianabeatriz13@gmail.com>
2024-10-11 09:33:21 -0300

    Commit final

--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,10 @@ def postagem(request, id):
         "post": post,
     }
     return render(request, 'post.html', context)
+
+def referencia(request):
+    referencia = Post.objects.all()
+    context = {
+        "referencia": referencia,
+    }
+    return render(request, 'referencias.html', context)
